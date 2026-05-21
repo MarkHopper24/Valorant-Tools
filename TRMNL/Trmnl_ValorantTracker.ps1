d950d046395b4c3b365b674a32005653964bb202
Mark Hopper <106134632+MarkHopper24@users.noreply.github.com>
2026-05-20 20:02:31 -0500

    Update Trmnl_ValorantTracker.ps1

--- TRMNL/Trmnl_ValorantTracker.ps1
+++ TRMNL/Trmnl_ValorantTracker.ps1
@@ -43,7 +43,7 @@ Function Get-MatchHistory {
         [string]$username,
         [string]$tagline,
         [string]$region,
-        [int]$size = 11
+        [int]$size = 10
     )
 
     $uri = "https://api.henrikdev.xyz/valorant/v3/matches/$region/$username/$tagline`?size=$size"
@@ -143,7 +143,7 @@ Function Get-LastMatches {
     if (-not $region) { $region = "na" }
 
     $MatchesOutput = @()
-    $MatchHistory = Get-MatchHistory -username $username -tagline $tagline -region $region -size 11
+    $MatchHistory = Get-MatchHistory -username $username -tagline $tagline -region $region -size 10
 
     foreach ($match in $MatchHistory) {
         $match.players.all_players |
